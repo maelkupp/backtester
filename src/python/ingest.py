be1429352ca9904c2d6a49917763de729d743275
maelkupp <ml.kupp29@gmail.com>
2026-04-27 12:13:31 +0200

    removed venv from tracking

--- src/python/ingest.py
+++ src/python/ingest.py
@@ -1,6 +1,17 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib as plt
 
 def ingestion_script():
-    print("script")
+    ticker = yf.Ticker("AAPL")
+    df = ticker.history(
+        start = "2024-01-01",
+        end = "2024-12-31",
+        interval = "1d"
+    )
+
+    print(f"column {df.columns}")
+    df["Close"].plot()
 
 
 
